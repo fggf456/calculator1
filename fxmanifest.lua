88e32a95c1e985c5ffd39e356277564bf6eca109
fggf456 <98290006+fggf456@users.noreply.github.com>
2022-01-24 01:34:30 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'barbaronn'
+description 'http://discord.barbaronn-scripts.shop'
+version '1.0.0'
+
+client_script 'client.lua'
+files {'nui.html'}
+ui_page 'nui.html'
